b4bdb519fd29bac7f2e044139d23af1cbddd0174
Hillel Saal <remi.saal@gmail.com>
2020-05-20 13:15:29 +0200

    Changed GET /account/token to POST /account/token

--- src/api/account.ts
+++ src/api/account.ts
@@ -2,56 +2,56 @@ import { Router } from "express"
 import { container } from "tsyringe"
 import { AccountService } from "../domain/account/account_service"
 import {
-	EmailExists,
-	AccountCreationFailed,
-	EmailDoesNotExist,
-	InvalidInput,
+  EmailExists,
+  AccountCreationFailed,
+  EmailDoesNotExist,
+  InvalidInput,
 } from "../domain/account/account_failures"
 
 export default () => {
-	const router = Router()
+  const router = Router()
 
-	const account_service = container.resolve(AccountService)
+  const account_service = container.resolve(AccountService)
 
-	router.get("/token", async (req, res) => {
-		const { email, password } = req.body
+  router.post("/token", async (req, res) => {
+    const { email, password } = req.body
 
-		const result = await account_service.login(email, password)
+    const result = await account_service.login(email, password)
 
-		return result.fold(
-			(err) => {
-				switch (err.constructor) {
-					case InvalidInput:
-						return res.status(400).json(err.unwrap())
-					case EmailDoesNotExist:
-						return res.status(401).json(err.unwrap())
-					default:
-						throw new Error(`Unhandled case: ${err.constructor.name}`)
-				}
-			},
-			(dto) => res.status(200).json(dto)
-		)
-	})
+    return result.fold(
+      (err) => {
+        switch (err.constructor) {
+          case InvalidInput:
+            return res.status(400).json(err.unwrap())
+          case EmailDoesNotExist:
+            return res.status(401).json(err.unwrap())
+          default:
+            throw new Error(`Unhandled case: ${err.constructor.name}`)
+        }
+      },
+      (dto) => res.status(200).json(dto)
+    )
+  })
 
-	router.post("/", async (req, res) => {
-		const { email, password } = req.body
+  router.post("/", async (req, res) => {
+    const { email, password } = req.body
 
-		const result = await account_service.register(email, password)
+    const result = await account_service.register(email, password)
 
-		return result.fold(
-			(err) => {
-				switch (err.constructor) {
-					case EmailExists:
-						return res.status(400).json(err.unwrap())
-					case AccountCreationFailed:
-						return res.status(500).json(err.unwrap())
-					default:
-						throw new Error(`Unhandled case: ${err}`)
-				}
-			},
-			(register_info) => res.status(201).json(register_info)
-		)
-	})
+    return result.fold(
+      (err) => {
+        switch (err.constructor) {
+          case EmailExists:
+            return res.status(400).json(err.unwrap())
+          case AccountCreationFailed:
+            return res.status(500).json(err.unwrap())
+          default:
+            throw new Error(`Unhandled case: ${err}`)
+        }
+      },
+      (register_info) => res.status(201).json(register_info)
+    )
+  })
 
-	return router
+  return router
 }
--- src/domain/account/password.vo.ts
+++ src/domain/account/password.vo.ts
@@ -1,38 +1,37 @@
 import { ValueObject, ValueObjectProps } from "../core/value_object"
-import Result from "../core/result"
 import bcrypt from "bcryptjs"
 import Should from "../core/should"
 import { Hash } from "./hash.vo"
 
 export interface PasswordProps extends ValueObjectProps {
-	password: string
+  password: string
 }
 
 export class Password extends ValueObject<PasswordProps> {
-	private _hash?: Hash
+  private _hash?: Hash
 
-	static create(props: PasswordProps): Password {
-		const error = Should.not_be_null_or_undefined([
-			{ name: "Password", value: props.password },
-		])
-		if (error.has_some()) throw error.get_val()
+  static create(props: PasswordProps): Password {
+    const error = Should.not_be_null_or_undefined([
+      { name: "Password", value: props.password },
+    ])
+    if (error.has_some()) throw error.get_val()
 
-		if (props.password.length < 8) throw "Password is too short"
-		return new Password(props)
-	}
+    if (props.password.length < 8) throw "Password is too short"
+    return new Password(props)
+  }
 
-	compare_hash(hash: string): boolean {
-		return bcrypt.compareSync(this.value.password, hash)
-	}
+  compare_hash(hash: string): boolean {
+    return bcrypt.compareSync(this.value.password, hash)
+  }
 
-	get hash(): string {
-		if (!this._hash) {
-			const salt = bcrypt.genSaltSync(10)
-			this._hash = new Hash({
-				hash: bcrypt.hashSync(this.value.password, salt),
-			})
-		}
+  get hash(): string {
+    if (!this._hash) {
+      const salt = bcrypt.genSaltSync(10)
+      this._hash = new Hash({
+        hash: bcrypt.hashSync(this.value.password, salt),
+      })
+    }
 
-		return this._hash.hash
-	}
+    return this._hash.hash
+  }
 }
