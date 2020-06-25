f39dfe57abad6aff29813071d24293b3bb8603cb
Hillel Saal <remi.saal@gmail.com>
2020-06-25 14:22:17 +0000

    Fixed DTO

--- src/domain/watch/parent_dtos.ts
+++ src/domain/watch/parent_dtos.ts
@@ -2,30 +2,32 @@ import { IGeofence } from "../../data/watch/parent_model"
 import { Subscription } from "./subscription.e"
 
 interface ISubscription {
-	watch: string
-	geofences: IGeofence[]
+  watch: String
+  name: String
+  geofences: IGeofence[]
 }
 
 export interface SubscriptionsDTO {
-	subscriptions: ISubscription[]
+  subscriptions: ISubscription[]
 }
 
 export const build_subscriptions_dto = (
-	subscriptions: Subscription[]
+  subscriptions: Subscription[]
 ): SubscriptionsDTO => ({
-	subscriptions: subscriptions.map((subscription) => ({
-		watch: subscription.watch.to_string(),
-		geofences: subscription.geofences.map((geofence) => ({
-			latitude: geofence.latitude,
-			longitude: geofence.longitude,
-			radius: geofence.radius,
-			name: geofence.name,
-			notification: geofence.notification,
-			time_frames: geofence.time_frames.map((time_frame) => ({
-				day_of_week: time_frame.day_of_week,
-				from: time_frame.from,
-				to: time_frame.to,
-			})),
-		})),
-	})),
+  subscriptions: subscriptions.map((subscription) => ({
+    name: subscription.name,
+    watch: subscription.watch.to_string(),
+    geofences: subscription.geofences.map((geofence) => ({
+      latitude: geofence.latitude,
+      longitude: geofence.longitude,
+      radius: geofence.radius,
+      name: geofence.name,
+      notification: geofence.notification,
+      time_frames: geofence.time_frames.map((time_frame) => ({
+        day_of_week: time_frame.day_of_week,
+        from: time_frame.from,
+        to: time_frame.to,
+      })),
+    })),
+  })),
 })
