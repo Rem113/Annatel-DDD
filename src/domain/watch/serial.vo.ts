74f211472f41675d1083f82198bbb061f3613168
Hillel Saal <remi.saal@gmail.com>
2020-07-05 21:47:05 +0000

    Serial can now be greater than 9 chars

--- src/domain/watch/serial.vo.ts
+++ src/domain/watch/serial.vo.ts
@@ -2,24 +2,24 @@ import { ValueObjectProps, ValueObject } from "../core/value_object"
 import Should from "../core/should"
 
 export interface SerialProps extends ValueObjectProps {
-	serial: string
+  serial: string
 }
 
 export class Serial extends ValueObject<SerialProps> {
-	static create(props: SerialProps): Serial {
-		const error = Should.not_be_null_or_undefined([
-			{
-				name: "Serial",
-				value: props.serial,
-			},
-		])
-		if (error.has_some()) throw error.get_val()
+  static create(props: SerialProps): Serial {
+    const error = Should.not_be_null_or_undefined([
+      {
+        name: "Serial",
+        value: props.serial,
+      },
+    ])
+    if (error.has_some()) throw error.get_val()
 
-		if (props.serial.length !== 9) throw "Serial number is invalid"
-		return new Serial(props)
-	}
+    if (props.serial.length < 9) throw "Serial number is invalid"
+    return new Serial(props)
+  }
 
-	get serial(): string {
-		return this.value.serial
-	}
+  get serial(): string {
+    return this.value.serial
+  }
 }
